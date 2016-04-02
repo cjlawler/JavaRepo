211334b2e8b5d523e6378f7f304628496f1e2f01
Conor Lawler <conorlawler@gmail.com>
2016-04-03 00:18:06 +0100

    Initial commit of test Java project

---
+++ JavaTest/src/org/cjl/test/TestClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.cjl.test;
+
+/**
+ * @author Conor
+ *
+ */
+public class TestClass
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("This is a test");
+	}
+
+}
