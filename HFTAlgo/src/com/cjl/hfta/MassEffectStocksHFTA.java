57737e9d01ca29bb6f918c7602ab1c0823f10c1f
Conor Lawler <conorlawler@gmail.com>
2016-07-26 17:45:51 +0100

    Initial commit of HFTA

---
+++ HFTAlgo/src/com/cjl/hfta/MassEffectStocksHFTA.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.cjl.hfta;
+
+/**
+ * @author Conor
+ *
+ */
+public class MassEffectStocksHFTA {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
